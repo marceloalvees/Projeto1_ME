cf120e7808bba8afe497eede971847db9af36b7d
Fabiana Sayuri <fabianakamamoto@hotmail.com>
2022-06-24 14:34:54 -0300

    arrumando o local de trabalho do program

--- conversorDeMoedas/Program.cs
+++ conversorDeMoedas/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace conversorDeMoedas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bem-vindo(a) ao Convertor de Moedas :)");
+
+
+        }
+    }
+}
